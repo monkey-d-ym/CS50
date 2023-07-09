3314fec86194aaa6897e3d031e5464f83ccccaf2
monkey-d-ym <138496179+monkey-d-ym@users.noreply.github.com>
2023-07-10 01:37:31 +0900

    Update indoor.py

--- CS50Python/Week0/indoor.py
+++ CS50Python/Week0/indoor.py
@@ -1,11 +1,6 @@
 """
 Indoor Voice
-
-WRITING IN ALL CAPS IS LIKE YELLING.
-Best to use your “indoor voice” sometimes, writing entirely in lowercase.
-In a file called indoor.py, implement a program in Python that prompts the user for input and then outputs that same input in lowercase. 
-Punctuation and whitespace should be outputted unchanged. 
-You’re welcome, but not required, to prompt the user explicitly, as by passing a str of your own as an argument to input.
+https://cs50.harvard.edu/python/2022/psets/0/indoor/
 """
 
 input_str = input("Write something: ")
