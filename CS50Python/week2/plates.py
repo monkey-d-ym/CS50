7d7f5b44d36f8d7458622c0b525a7ac30d5120bf
monkey-d-ym <138496179+monkey-d-ym@users.noreply.github.com>
2023-07-10 01:51:26 +0900

    Create plates.py

---
+++ CS50Python/week2/plates.py
@@ -0,0 +1,24 @@
+"""
+Vanity Plates
+https://cs50.harvard.edu/python/2022/psets/2/plates/
+"""
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+def is_valid(s):
+    if 2 <= len(s) <= 6 and s[0:2].isalpha():
+        if s.isalpha():
+            return True
+        elif s.isalnum():
+            for i in range(len(s)):
+                if s[i].isdigit():
+                    if s[i:].isdigit() and s[i] != "0":
+                        return True
+                    else:
+                        return False
+
+main()
