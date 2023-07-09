b4a7abb1c48e5e2d5607387533773d2b7464c29e
monkey-d-ym <138496179+monkey-d-ym@users.noreply.github.com>
2023-07-10 01:38:00 +0900

    Update playback.py

--- CS50Python/Week0/playback.py
+++ CS50Python/Week0/playback.py
@@ -1,10 +1,6 @@
 """
 Playback Speed
-
-Some people have a habit of lecturing speaking rather quickly, and it’d be nice to slow them down, 
-a la YouTube’s 0.75 playback speed, or even by having them pause between words.
-In a file called playback.py, implement a program in Python that prompts the user for input and then outputs that same input, 
-replacing each space with ... (i.e., three periods).
+https://cs50.harvard.edu/python/2022/psets/0/playback/
 """
 input_str = input("Write something: ")
 output_str = input_str.replace(" ","...")
