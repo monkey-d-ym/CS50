fd2438c3d59bee976ecf0f52445e4e88a56496ad
Ye Min <138496179+monkey-d-ym@users.noreply.github.com>
2023-08-14 16:25:01 +0900

    Create test_bank.py

---
+++ CS50Python/week5/test_bank.py
@@ -0,0 +1,31 @@
+"""
+Back to the Bank
+https://cs50.harvard.edu/python/2022/psets/5/test_bank/
+"""
+
+from bank import value
+
+def test_hello():
+    assert value("Hello, how are you?") == 0
+def test_h():
+    assert value("How are you?") == 20
+def test_noth():
+    assert value("What's your name?") == 100
+
+"""
+def main():
+    greeting = input("Greeting: ").strip().lower()
+    print(f"${value(greeting)}")
+
+
+def value(greeting):
+    if greeting.startswith("hello") == 1:
+        return 0
+    elif greeting[0] == "h":
+        return 20
+    else:
+        return 100
+
+if __name__ == "__main__":
+    main()
+"""
